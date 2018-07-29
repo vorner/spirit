39ffeab8a002f6db64fd80a9fc80414eb022e1cb
Michal 'vorner' Vaner <vorner@vorner.cz>
2018-07-29 21:24:11 +0200

    Config validation support

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,12 @@
     html_root_url = "https://docs.rs/spirit/0.1.0/spirit/",
     test(attr(deny(warnings)))
 )]
+#![cfg_attr(feature = "cargo-clippy", allow(type_complexity))]
 // #![deny(missing_docs, warnings)] XXX
 
 extern crate arc_swap;
 extern crate config;
+#[macro_use]
 extern crate failure;
 extern crate libc;
 #[macro_use]
@@ -21,9 +23,10 @@ extern crate structopt;
 use std::borrow::Borrow;
 use std::collections::{HashMap, HashSet};
 use std::ffi::OsString;
-use std::fmt::Debug;
+use std::fmt::{Debug, Formatter, Result as FmtResult};
 use std::marker::PhantomData;
 use std::path::{Path, PathBuf};
+use std::slice::Iter;
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::thread;
@@ -88,6 +91,168 @@ pub fn log_errors<R, F: FnOnce() -> Result<R, Error>>(f: F) -> Result<R, Error> 
     result
 }
 
+// TODO: Should this be open enum?
+// TODO: Move to validation submodule and drop the prefix?
+#[derive(Copy, Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
+pub enum ValidationLevel {
+    Nothing,
+    Hint,
+    Warning,
+    Error,
+}
+
+impl Default for ValidationLevel {
+    fn default() -> Self {
+        ValidationLevel::Nothing
+    }
+}
+
+#[derive(Default)]
+pub struct ValidationResult {
+    level: ValidationLevel,
+    description: String,
+    on_abort: Option<Box<FnMut() + Send + Sync>>,
+    on_success: Option<Box<FnMut() + Send + Sync>>,
+}
+
+impl ValidationResult {
+    pub fn new<S: Into<String>>(level: ValidationLevel, s: S) -> Self {
+        ValidationResult {
+            level,
+            description: s.into(),
+            .. Default::default()
+        }
+    }
+    pub fn nothing() -> Self {
+        Self::new(ValidationLevel::Nothing, "")
+    }
+    pub fn hint<S: Into<String>>(s: S) -> Self {
+        Self::new(ValidationLevel::Hint, s)
+    }
+    pub fn warning<S: Into<String>>(s: S) -> Self {
+        Self::new(ValidationLevel::Warning, s)
+    }
+    pub fn error<S: Into<String>>(s: S) -> Self {
+        Self::new(ValidationLevel::Error, s)
+    }
+    pub fn level(&self) -> ValidationLevel {
+        self.level
+    }
+    pub fn description(&self) -> &str {
+        &self.description
+    }
+    pub fn on_success<F: FnOnce() + Send + Sync + 'static>(self, f: F) -> Self {
+        let mut f = Some(f);
+        let wrapper = move || (f.take().unwrap())();
+        Self {
+            on_success: Some(Box::new(wrapper)),
+            .. self
+        }
+    }
+    pub fn on_abort<F: FnOnce() + Send + Sync + 'static>(self, f: F) -> Self {
+        let mut f = Some(f);
+        let wrapper = move || (f.take().unwrap())();
+        Self {
+            on_success: Some(Box::new(wrapper)),
+            .. self
+        }
+    }
+}
+
+impl Debug for ValidationResult {
+    fn fmt(&self, fmt: &mut Formatter) -> FmtResult {
+        fmt.debug_struct("ValidationResult")
+            .field("level", &self.level)
+            .field("description", &self.description)
+            .field("on_abort", if self.on_abort.is_some() { &"Fn()" } else { &"None" })
+            .field("on_success", if self.on_success.is_some() { &"Fn()" } else { &"None" })
+            .finish()
+    }
+}
+
+impl From<String> for ValidationResult {
+    fn from(s: String) -> Self {
+        ValidationResult {
+            level: ValidationLevel::Error,
+            description: s,
+            .. Default::default()
+        }
+    }
+}
+
+impl From<&'static str> for ValidationResult {
+    fn from(s: &'static str) -> Self {
+        ValidationResult {
+            level: ValidationLevel::Error,
+            description: s.to_owned(),
+            .. Default::default()
+        }
+    }
+}
+
+impl From<(ValidationLevel, String)> for ValidationResult {
+    fn from((level, s): (ValidationLevel, String)) -> Self {
+        ValidationResult {
+            level,
+            description: s,
+            .. Default::default()
+        }
+    }
+}
+
+impl From<(ValidationLevel, &'static str)> for ValidationResult {
+    fn from((level, s): (ValidationLevel, &'static str)) -> Self {
+        ValidationResult {
+            level,
+            description: s.to_owned(),
+            .. Default::default()
+        }
+    }
+}
+
+#[derive(Debug, Default, Fail)]
+#[fail(display = "Validation failed")] // TODO: Something better
+pub struct ValidationResults(Vec<ValidationResult>);
+
+impl ValidationResults {
+    pub fn new() -> Self {
+        Self::default()
+    }
+    pub fn merge<R: Into<ValidationResults>>(&mut self, other: R) {
+        self.0.extend(other.into().0);
+    }
+    pub fn iter(&self) -> Iter<ValidationResult> {
+        self.into_iter()
+    }
+    pub fn max_level(&self) -> Option<ValidationLevel> {
+        self.iter().map(|r| r.level).max()
+    }
+}
+
+impl<'a> IntoIterator for &'a ValidationResults {
+    type Item = &'a ValidationResult;
+    type IntoIter = Iter<'a, ValidationResult>;
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.iter()
+    }
+}
+
+impl From<ValidationResult> for ValidationResults {
+    fn from(val: ValidationResult) -> Self {
+        ValidationResults(vec![val])
+    }
+}
+
+impl<I, It> From<I> for ValidationResults
+where
+    I: IntoIterator<Item = It>,
+    It: Into<ValidationResult>,
+{
+    fn from(vals: I) -> Self {
+        ValidationResults(vals.into_iter().map(Into::into).collect())
+    }
+}
+
 pub struct Spirit<S, O = (), C = ()>
 where
     S: Borrow<ArcSwap<C>> + 'static,
@@ -100,6 +265,7 @@ where
     config_env: Option<String>,
     config_filter: Box<Fn(&Path) -> bool + Sync + Send>,
     config_hooks: Vec<Box<Fn(&Arc<C>) + Sync + Send>>,
+    config_validators: Vec<Box<Fn(&Arc<C>, &Arc<C>, &O) -> ValidationResults + Sync + Send>>,
     // TODO: Validation
     opts: O,
     sig_hooks: HashMap<libc::c_int, Vec<Box<Fn() + Sync + Send>>>,
@@ -121,6 +287,7 @@ where
             config_env: None,
             config_hooks: Vec::new(),
             config_filter: Box::new(|_| true),
+            config_validators: Vec::new(),
             opts: PhantomData,
             sig_hooks: HashMap::new(),
             terminate_hooks: Vec::new(),
@@ -132,7 +299,35 @@ where
     }
 
     pub fn config_reload(&self) -> Result<(), Error> {
-        unimplemented!();
+        let config = self.load_config()?;
+        let new = Arc::new(config.config);
+        let old = self.config.borrow().load();
+        let mut results = self.config_validators
+            .iter()
+            .map(|v| v(&old, &new, &self.opts))
+            .fold(ValidationResults::new(), |mut acc, r| {
+                acc.merge(r);
+                acc
+            });
+        // TODO: Log the stuff in results
+        if results.max_level() == Some(ValidationLevel::Error) {
+            for r in &mut results.0 {
+                if let Some(abort) = r.on_abort.as_mut() {
+                    abort();
+                }
+            }
+            return Err(results.into())
+        }
+        for r in &mut results.0 {
+            if let Some(success) = r.on_success.as_mut() {
+                success();
+            }
+        }
+        self.config.borrow().store(Arc::clone(&new));
+        for hook in &self.config_hooks {
+            hook(&new);
+        }
+        Ok(())
     }
 
     pub fn is_terminated(&self) -> bool {
@@ -202,13 +397,6 @@ where
         Ok(config.try_into()?)
     }
 
-    fn invoke_config_hooks(&self) {
-        let cfg = self.config.borrow().load();
-        for hook in &self.config_hooks {
-            hook(&cfg);
-        }
-    }
-
 }
 
 pub struct Builder<S, O, C> {
@@ -217,6 +405,7 @@ pub struct Builder<S, O, C> {
     config_env: Option<String>,
     config_hooks: Vec<Box<Fn(&Arc<C>) + Sync + Send>>,
     config_filter: Box<Fn(&Path) -> bool + Sync + Send>,
+    config_validators: Vec<Box<Fn(&Arc<C>, &Arc<C>, &O) -> ValidationResults + Sync + Send>>,
     opts: PhantomData<O>,
     sig_hooks: HashMap<libc::c_int, Vec<Box<Fn() + Sync + Send>>>,
     terminate_hooks: Vec<Box<Fn() + Sync + Send>>,
@@ -247,15 +436,14 @@ where
             config_env: self.config_env,
             config_hooks: self.config_hooks,
             config_filter: self.config_filter,
+            config_validators: self.config_validators,
             opts: opts.other,
             sig_hooks: self.sig_hooks,
             terminate: AtomicBool::new(false),
             terminate_hooks: self.terminate_hooks,
         };
+        spirit.config_reload()?;
         let signals = Signals::new(interesting_signals)?;
-        let config = spirit.load_config()?;
-        spirit.config.borrow().store(Arc::new(config.config));
-        spirit.invoke_config_hooks();
         let spirit = Arc::new(spirit);
         let spirit_bc = Arc::clone(&spirit);
         thread::Builder::new()
@@ -298,6 +486,20 @@ where
     pub fn config_filter<F: Fn(&Path) -> bool + Sync + Send + 'static>(self, filter: F) -> Self {
         Self {
             config_filter: Box::new(filter),
+            .. self
+        }
+    }
+
+    pub fn config_validator<R, F>(self, f: F) -> Self
+    where
+        F: Fn(&Arc<C>, &Arc<C>, &O) -> R + Sync + Send + 'static,
+        R: Into<ValidationResults>,
+    {
+        let wrapper = move |old: &Arc<C>, new: &Arc<C>, opts: &O| f(old, new, opts).into();
+        let mut validators = self.config_validators;
+        validators.push(Box::new(wrapper));
+        Self {
+            config_validators: validators,
             .. self
         }
     }
