a6f77758d69f6426f69497bfbe19503ac2849a27
Michal 'vorner' Vaner <vorner@vorner.cz>
2018-09-18 21:26:37 +0200

    hyper: Graceful shutdown of connections

--- spirit-hyper/src/lib.rs
+++ spirit-hyper/src/lib.rs
@@ -18,23 +18,25 @@ extern crate structopt;
 extern crate tokio_io;
 
 use std::borrow::Borrow;
+use std::error::Error;
 use std::fmt::{Debug, Display};
 use std::sync::Arc;
 
 use arc_swap::ArcSwap;
-use failure::Error;
-use futures::{Future, IntoFuture};
-use hyper::server::conn::Http;
+use failure::Error as FailError;
+use futures::future::Shared;
+use futures::sync::oneshot::{self, Receiver};
+use futures::{Async, Future, IntoFuture, Poll};
+use hyper::body::Payload;
+use hyper::server::conn::{Connection, Http};
 use hyper::service::Service;
-use hyper::{Body, Request, Response};
+use hyper::Body;
 use serde::Deserialize;
 use spirit::helpers::IteratedCfgHelper;
 use spirit::{Builder, Empty, Spirit};
 use spirit_tokio::{ResourceMaker, TcpListen};
 use structopt::StructOpt;
 use tokio_io::{AsyncRead, AsyncWrite};
-
-// TODO: Graceful shutdown of the connections
 
 #[derive(Clone, Debug, Default, Deserialize, Eq, PartialEq, Ord, PartialOrd, Hash)]
 pub struct HyperServer<Transport> {
@@ -43,6 +45,37 @@ pub struct HyperServer<Transport> {
 }
 
 pub type HttpServer<ExtraCfg = Empty> = HyperServer<TcpListen<ExtraCfg>>;
+
+struct ShutdownConn<T, S: Service> {
+    conn: Connection<T, S>,
+    shutdown: Option<Shared<Receiver<()>>>,
+}
+
+impl<T, S, B> Future for ShutdownConn<T, S>
+where
+    S: Service<ReqBody = Body, ResBody = B> + 'static,
+    S::Error: Into<Box<Error + Send + Sync>>,
+    S::Future: Send,
+    T: AsyncRead + AsyncWrite + 'static,
+    B: Payload + 'static,
+{
+    type Item = <Connection<T, S> as Future>::Item;
+    type Error = <Connection<T, S> as Future>::Error;
+    fn poll(&mut self) -> Poll<Self::Item, Self::Error> {
+        let do_shutdown = self.shutdown
+            .as_mut()
+            .map(Future::poll)
+            .unwrap_or(Ok(Async::NotReady));
+        match do_shutdown {
+            Ok(Async::NotReady) => (), // Don't shutdown yet (or already done that)
+            _ => {
+                self.conn.graceful_shutdown();
+                self.shutdown.take();
+            }
+        }
+        self.conn.poll()
+    }
+}
 
 impl<S, O, C, Transport, Action, ActionFut, Srv, H> IteratedCfgHelper<S, O, C, Action>
     for HyperServer<Transport>
@@ -53,7 +86,7 @@ where
     Transport: ResourceMaker<S, O, C, ()>,
     Transport::Resource: AsyncRead + AsyncWrite + Send + 'static,
     Action: Fn(&Arc<Spirit<S, O, C>>, &Transport::ExtraCfg) -> ActionFut + Sync + Send + 'static,
-    ActionFut: IntoFuture<Item = (Srv, H), Error = Error>,
+    ActionFut: IntoFuture<Item = (Srv, H), Error = FailError>,
     ActionFut::Future: Send + 'static,
     Srv: Service<ReqBody = Body> + Send + 'static,
     Srv::Future: Send,
@@ -70,13 +103,19 @@ where
         ExtractedIter: IntoIterator<Item = Self>,
         Name: Clone + Display + Send + Sync + 'static,
     {
+        let (shutdown_send, shutdown_recv) = oneshot::channel::<()>();
+        let shutdown_recv = shutdown_recv.shared();
         let inner_action = move |spirit: &_, resource, extra_cfg: &_, _: &()| {
+            let shutdown_recv = shutdown_recv.clone();
             action(spirit, extra_cfg)
                 .into_future()
                 .and_then(|(srv, http)| {
-                    http.borrow()
-                        .serve_connection(resource, srv)
-                        .map_err(Error::from)
+                    let conn = http.borrow().serve_connection(resource, srv);
+                    let conn = ShutdownConn {
+                        shutdown: Some(shutdown_recv),
+                        conn,
+                    };
+                    conn.map_err(FailError::from)
                 })
         };
         let inner_extractor = move |cfg: &_| {
@@ -84,6 +123,11 @@ where
                 .into_iter()
                 .map(|instance| (instance.transport, ()))
         };
-        Transport::apply(inner_extractor, inner_action, name, builder)
+        let mut shutdown_send = Some(shutdown_send);
+        Transport::apply(inner_extractor, inner_action, name, builder).on_terminate(move || {
+            if let Some(send) = shutdown_send.take() {
+                let _ = send.send(());
+            }
+        })
     }
 }
