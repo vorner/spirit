9ec3c0f509325bb30c34cc0bdd8c048bebab7289
Michal 'vorner' Vaner <vorner@vorner.cz>
2018-12-09 12:41:40 +0100

    Improve docs around Empty

--- src/lib.rs
+++ src/lib.rs
@@ -260,6 +260,9 @@ pub struct MissingFile(PathBuf);
 /// When the application doesn't need the configuration (in excess of the automatic part provided
 /// by this library) or it doesn't need any command line options of its own, this struct can be
 /// used to plug the type parameter.
+///
+/// Other places (eg. around helpers) may use this to plug a type parameter that isn't needed, do
+/// nothing or something like that.
 #[derive(
     Copy, Clone, Debug, Default, Deserialize, Eq, PartialEq, Hash, Ord, PartialOrd, StructOpt,
 )]
