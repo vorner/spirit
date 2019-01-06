29b8cc858c780a4733444b29d44c44f95ad678b5
Michal 'vorner' Vaner <vorner@vorner.cz>
2019-01-06 16:09:44 +0100

    Fragment for Vec<Fragment> and similar
    Hopefully… needs to get tested somehow, there's a bunch of moving parts
    around.
    
    And yes, documented, logged…

--- src/fragment.rs
+++ src/fragment.rs
@@ -1,7 +1,11 @@
-use std::collections::HashMap;
+use std::collections::{BTreeSet, BinaryHeap, HashMap, HashSet, LinkedList};
+use std::hash::{BuildHasher, Hash};
+use std::iter;
 use std::marker::PhantomData;
+use std::mem;
 use std::sync::Arc;
 
+use either::Either;
 use failure::Error;
 use parking_lot::Mutex;
 
@@ -30,7 +34,10 @@ impl Iterator for IdGen {
     type Item = CacheId;
     fn next(&mut self) -> Option<CacheId> {
         let id = self.0;
-        self.0 = self.0.checked_add(1).expect("WTF? Run out of 128bit cache IDs!?");
+        self.0 = self
+            .0
+            .checked_add(1)
+            .expect("WTF? Run out of 128bit cache IDs!?");
         Some(CacheId(id))
     }
 }
@@ -62,6 +69,7 @@ pub trait Driver<F: Fragment> {
         T: Transformation<<Self::SubFragment as Fragment>::Resource, I, Self::SubFragment>;
     fn confirm(&mut self);
     fn abort(&mut self);
+    fn maybe_cached(&self, frament: &F) -> bool;
 }
 
 #[derive(Clone, Copy, Debug, Default)]
@@ -92,6 +100,190 @@ impl<F: Fragment> Driver<F> for TrivialDriver {
     }
     fn confirm(&mut self) {}
     fn abort(&mut self) {}
+    fn maybe_cached(&self, _: &F) -> bool {
+        false
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+// TODO: Use some kind of immutable/persistent data structures? Or not, this is likely to be small?
+pub struct IdMapping {
+    mapping: HashMap<CacheId, CacheId>,
+}
+
+impl IdMapping {
+    pub fn translate<'a, R, I>(&'a mut self, id_gen: &'a mut IdGen, instructions: I)
+        -> impl Iterator<Item = CacheInstruction<R>> + 'a
+    where
+        R: 'a,
+        I: IntoIterator<Item = CacheInstruction<R>> + 'a,
+    {
+        instructions
+            .into_iter()
+            // Borrow checker notes:
+            // We need to move the self and id_gen into the closure. Otherwise, it creates
+            // &mut &mut IdGen monster behind the scenes, however, the outer &mut has a short
+            // lifetime because it points to the function's parameter.
+            //
+            // The mem::swap with the HashMap below is also because of borrow checker. The drain we
+            // would like to use instead would have to eat the `&mut self` (or borrow it, but see
+            // the same problem above). We *know* that we won't call this again until the drain
+            // iterator is wholly consumed by flat_map, but the borrow checker doesn't. So this
+            // trick instead.
+            .flat_map(move |i| match i {
+                CacheInstruction::DropAll => {
+                    let mut mapping = HashMap::new();
+                    mem::swap(&mut mapping, &mut self.mapping);
+                    Either::Left(mapping
+                        .into_iter()
+                        .map(|(_, outer_id)| CacheInstruction::DropSpecific(outer_id)))
+                },
+                CacheInstruction::DropSpecific(id) => {
+                    let id = self.mapping
+                        .remove(&id)
+                        .expect("Inconsistent use of cache: missing ID to remove");
+                    Either::Right(iter::once(CacheInstruction::DropSpecific(id)))
+                }
+                CacheInstruction::Install { id, resource } => {
+                    let new_id = id_gen.next().expect("Run out of cache IDs? Impossible");
+                    assert!(self.mapping.insert(id, new_id).is_none(), "Duplicate ID created");
+                    Either::Right(iter::once(CacheInstruction::Install { id: new_id, resource }))
+                }
+            })
+    }
+}
+
+#[derive(Debug, Default)]
+struct ItemDriver<Driver> {
+    driver: Driver,
+    id_mapping: IdMapping,
+    proposed_mapping: Option<IdMapping>,
+    used: bool,
+    new: bool,
+}
+
+#[derive(Debug)]
+pub struct SeqDriver<Item, SlaveDriver> {
+    id_gen: IdGen,
+    sub_drivers: Vec<ItemDriver<SlaveDriver>>,
+    transaction_open: bool,
+    // TODO: Can we actually get rid of this?
+    _item: PhantomData<Fn(&Item)>,
+}
+
+// The derived Default balks on Item: !Default, but we *don't* need that
+impl<Item, SlaveDriver> Default for SeqDriver<Item, SlaveDriver> {
+    fn default() -> Self {
+        Self {
+            id_gen: IdGen::new(),
+            sub_drivers: Vec::new(),
+            transaction_open: false,
+            _item: PhantomData,
+        }
+    }
+}
+
+// TODO: This one is complex enough, this calls for bunch of trace and debug logging!
+impl<F, I, SlaveDriver> Driver<F> for SeqDriver<I, SlaveDriver>
+where
+    F: Fragment,
+    // TODO: This could be generalized to ToOwned, right?
+    I: Fragment,
+    for<'a> &'a F: IntoIterator<Item = &'a I>,
+    SlaveDriver: Driver<I> + Default,
+{
+    type SubFragment = SlaveDriver::SubFragment;
+    fn instructions<T, Ins>(
+        &mut self,
+        fragment: &F,
+        transform: &mut T,
+        name: &str,
+    ) -> Result<Vec<CacheInstruction<T::OutputResource>>, Vec<Error>>
+    where
+        T: Transformation<<Self::SubFragment as Fragment>::Resource, Ins, Self::SubFragment>,
+    {
+        assert!(!self.transaction_open);
+        self.transaction_open = true;
+        let mut instructions = Vec::new();
+        let mut errors = Vec::new();
+
+        for sub in fragment {
+            let existing = self
+                .sub_drivers
+                .iter_mut()
+                .find(|d| !d.used && d.driver.maybe_cached(sub));
+            // unwrap_or_else angers the borrow checker here
+            let slot = if let Some(existing) = existing {
+                existing
+            } else {
+                self.sub_drivers.push(ItemDriver::default());
+                let slot = self.sub_drivers.last_mut().unwrap();
+                slot.new = true;
+                slot
+            };
+
+            slot.used = true;
+            match slot.driver.instructions(sub, transform, name) {
+                Ok(new_instructions) => {
+                    let mapping = if slot.new {
+                        &mut slot.id_mapping
+                    } else {
+                        slot.proposed_mapping = Some(slot.id_mapping.clone());
+                        slot.proposed_mapping.as_mut().unwrap()
+                    };
+                    instructions.extend(mapping.translate(&mut self.id_gen, new_instructions));
+                }
+                Err(errs) => errors.extend(errs),
+            }
+        }
+
+        if errors.is_empty() {
+            Ok(instructions)
+        } else {
+            self.abort();
+            Err(errors)
+        }
+    }
+    fn confirm(&mut self) {
+        assert!(self.transaction_open);
+        self.transaction_open = false;
+        // Get rid of the unused ones
+        self.sub_drivers.retain(|s| s.used);
+        // Confirm all the used ones, accept proposed mappings and mark everything as old for next
+        // round.
+        for sub in &mut self.sub_drivers {
+            sub.driver.confirm();
+            if let Some(mapping) = sub.proposed_mapping.take() {
+                sub.id_mapping = mapping;
+            }
+            sub.new = false;
+        }
+    }
+    fn abort(&mut self) {
+        assert!(self.transaction_open);
+        self.transaction_open = false;
+        // Get rid of the new ones completely
+        self.sub_drivers.retain(|s| !s.new);
+        // Abort anything we touched before
+        for sub in &mut self.sub_drivers {
+            if sub.used {
+                sub.driver.abort();
+                sub.proposed_mapping.take();
+                sub.used = false;
+            }
+            assert!(
+                sub.proposed_mapping.is_none(),
+                "Proposed mapping for something not used"
+            );
+        }
+    }
+    fn maybe_cached(&self, fragment: &F) -> bool {
+        fragment.into_iter().any(|s| {
+            self.sub_drivers
+                .iter()
+                .any(|slave| slave.driver.maybe_cached(s))
+        })
+    }
 }
 
 pub trait Installer<Resource, O, C>: Default {
@@ -99,6 +291,28 @@ pub trait Installer<Resource, O, C>: Default {
     fn install(&mut self, resource: Resource) -> Self::UninstallHandle;
     fn init<B: Extensible<Opts = O, Config = C>>(&mut self, builder: B) -> Result<B, Error> {
         Ok(builder)
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct SeqInstaller<Slave> {
+    slave: Slave,
+}
+
+impl<Resource, O, C, Slave> Installer<Resource, O, C> for SeqInstaller<Slave>
+where
+    Resource: IntoIterator,
+    Slave: Installer<Resource::Item, O, C>,
+{
+    type UninstallHandle = Vec<Slave::UninstallHandle>;
+    fn install(&mut self, resource: Resource) -> Self::UninstallHandle {
+        resource
+            .into_iter()
+            .map(|r| self.slave.install(r))
+            .collect()
+    }
+    fn init<B: Extensible<Opts = O, Config = C>>(&mut self, builder: B) -> Result<B, Error> {
+        self.slave.init(builder)
     }
 }
 
@@ -134,6 +348,9 @@ where
     }
 }
 
+// Marker trait...
+pub trait Stackable {}
+
 pub trait Fragment: Sized {
     type Driver: Driver<Self> + Default;
     type Installer: Default;
@@ -147,6 +364,46 @@ pub trait Fragment: Sized {
     }
 }
 
+// TODO: Export the macro for other containers?
+macro_rules! fragment_for_seq {
+    ($container: ident<$base: ident $(, $extra: ident)*> $(where $($bounds: tt)+)*) => {
+        impl<$base: Clone + Fragment + Stackable + 'static $(, $extra)*> Fragment
+            for $container<$base $(, $extra)*>
+        $(
+            where
+            $($bounds)+
+        )*
+        {
+            type Driver = SeqDriver<$base, $base::Driver>;
+            type Installer = SeqInstaller<$base::Installer>;
+            type Seed = Vec<$base::Seed>;
+            type Resource = Vec<$base::Resource>;
+            fn make_seed(&self, name: &str) -> Result<Self::Seed, Error> {
+                self.iter().map(|i| i.make_seed(name)).collect()
+            }
+            fn make_resource(&self, seed: &mut Self::Seed, name: &str)
+                -> Result<Self::Resource, Error>
+            {
+                self.iter()
+                    .zip(seed)
+                    .map(|(i, s)| i.make_resource(s, name))
+                    .collect()
+            }
+        }
+    }
+}
+
+fragment_for_seq!(Vec<T>);
+fragment_for_seq!(BTreeSet<T>);
+fragment_for_seq!(LinkedList<T>);
+fragment_for_seq!(Option<T>);
+fragment_for_seq!(BinaryHeap<T> where T: Ord);
+fragment_for_seq!(HashSet<T, S> where T: Eq + Hash, S: BuildHasher);
+
+// TODO: How do we stack maps, etc?
+// TODO: Arcs, Rcs, Mutexes, refs, ...
+
+// TODO: Make this into a macro instead, so we can impl Fragment for refs?
 pub trait SimpleFragment: Sized {
     type SimpleResource;
     type SimpleInstaller: Default;
@@ -404,13 +661,14 @@ where
         let mut extractor = self.extractor;
         let validator = move |_old: &_, cfg: &mut B::Config, opts: &B::Opts| -> ValidationResults {
             let fragment = extractor.extract(opts, cfg);
-            let instructions = match driver
-                .lock()
-                .instructions(&fragment, &mut transformation, name)
-            {
-                Ok(i) => i,
-                Err(errs) => return errs.into(),
-            };
+            let instructions =
+                match driver
+                    .lock()
+                    .instructions(&fragment, &mut transformation, name)
+                {
+                    Ok(i) => i,
+                    Err(errs) => return errs.into(),
+                };
             let driver_f = Arc::clone(&driver);
             let failure = move || {
                 driver_f.lock().abort();
